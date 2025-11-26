8c750f78595b2b4d9de4cc912d1ec0a15203a454
dotsenkois <dotsenkois@yandex.ru>
2025-11-26 22:43:03 +0700

    Использование клиента

--- news/tests/test_trial.py
+++ news/tests/test_trial.py
@@ -1,27 +1,20 @@
-# news/tests/test_trial.py
-from django.test import TestCase
+# Импортируем функцию для определения модели пользователя.
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
 
-from news.models import News
+# Получаем модель пользователя.
+User = get_user_model()
 
 
 class TestNews(TestCase):
-    # Все нужные переменные сохраняем в атрибуты класса.
-    TITLE = 'Заголовок новости'
-    TEXT = 'Тестовый текст'
-    
+
     @classmethod
     def setUpTestData(cls):
-        cls.news = News.objects.create(
-            # При создании объекта обращаемся к константам класса через cls.
-            title=cls.TITLE,
-            text=cls.TEXT,
-        )
-
-    def test_successful_creation(self):
-        news_count = News.objects.count()
-        self.assertEqual(news_count, 1)
-
-    def test_title(self):
-        # Чтобы проверить равенство с константой -
-        # обращаемся к ней через self, а не через cls:
-        self.assertEqual(self.news.title, self.TITLE)
+        # Создаём пользователя.
+        cls.user = User.objects.create(username='testUser')
+        # Создаём объект клиента.
+        cls.user_client = Client()
+        # "Логинимся" в клиенте при помощи метода force_login().        
+        cls.user_client.force_login(cls.user)
+        # Теперь через этот клиент можно отправлять запросы
+        # от имени пользователя с логином "testUser".
