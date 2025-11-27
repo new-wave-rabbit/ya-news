bfb98cd2312a5af747b91ebd4dcc6e3b8d972d69
dotsenkois <dotsenkois@yandex.ru>
2025-11-27 14:51:35 +0700

    Добавлена генерация контента для тестирования контента

---
+++ news/tests/test_content.py
@@ -0,0 +1,16 @@
+# news/tests/test_content.py
+from django.conf import settings
+from django.test import TestCase
+
+from news.models import News
+
+
+class TestHomePage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        all_news = []
+        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
+            news = News(title=f'Новость {index}', text='Просто текст.')
+            all_news.append(news)
+        News.objects.bulk_create(all_news) 
