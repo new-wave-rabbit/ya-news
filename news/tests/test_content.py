8ecaec65516d9ece8e5a7a14e8f2448ec74b6d8e
dotsenkois <dotsenkois@yandex.ru>
2025-11-27 15:01:47 +0700

    Добавлен тест количества новостей

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,10 +1,16 @@
 # news/tests/test_content.py
+from datetime import datetime, timedelta 
+
 from django.conf import settings
 from django.test import TestCase
+from django.urls import reverse
 
 from news.models import News
 
+
+
 class TestHomePage(TestCase):
+    HOME_URL = reverse('news:home')
 
     @classmethod
     def setUpTestData(cls):
@@ -12,3 +18,14 @@ class TestHomePage(TestCase):
             News(title=f'Новость {index}', text='Просто текст.')
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
         )
+
+    def test_news_count(self):
+        # Загружаем главную страницу.
+        response = self.client.get(self.HOME_URL)
+        # Код ответа не проверяем, его уже проверили в тестах маршрутов.
+        # Получаем список объектов из словаря контекста.
+        object_list = response.context['object_list']
+        # Определяем количество записей в списке.
+        news_count = object_list.count()
+        # Проверяем, что на странице именно 10 новостей.
+        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE) 
