a9b75e3d070799c69daaf56356d48ffc0b5fb7ee
dotsenkois <dotsenkois@yandex.ru>
2025-11-28 22:27:09 +0700

    Добавлены тесты наличия формы в словаре контента

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -100,11 +100,12 @@ class TestDetailPage(TestCase):
     def test_anonymous_client_has_no_form(self):
         response = self.client.get(self.detail_url)
         self.assertNotIn('form', response.context)
-        
+
     def test_authorized_client_has_form(self):
         # Авторизуем клиент при помощи ранее созданного пользователя.
         self.client.force_login(self.author)
         response = self.client.get(self.detail_url)
         self.assertIn('form', response.context)
         # Проверим, что объект формы соответствует нужному классу формы.
-        self.assertIsInstance(response.context['form'], CommentForm) 
+        self.assertIsInstance(response.context['form'], CommentForm)
+
