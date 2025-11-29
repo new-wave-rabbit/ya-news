ee3bdaf3b9befd809bb937559d8c0a1a025b9ee9
dotsenkois <dotsenkois@yandex.ru>
2025-11-29 12:37:29 +0700

    тесты автора

--- news/tests/test_logic.py
+++ news/tests/test_logic.py
@@ -108,3 +108,44 @@ class TestCommentEditDelete(TestCase):
         cls.delete_url = reverse('news:delete', args=(cls.comment.id,))  
         # Формируем данные для POST-запроса по обновлению комментария.
         cls.form_data = {'text': cls.NEW_COMMENT_TEXT} 
+
+    def test_author_can_delete_comment(self):
+        # От имени автора комментария отправляем DELETE-запрос на удаление.
+        response = self.author_client.delete(self.delete_url)
+        # Проверяем, что редирект привёл к разделу с комментариями.
+        self.assertRedirects(response, self.url_to_comments)
+        # Заодно проверим статус-коды ответов.
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        # Считаем количество комментариев в системе.
+        comments_count = Comment.objects.count()
+        # Ожидаем ноль комментариев в системе.
+        self.assertEqual(comments_count, 0) 
+
+    def test_user_cant_delete_comment_of_another_user(self):
+        # Выполняем запрос на удаление от пользователя-читателя.
+        response = self.reader_client.delete(self.delete_url)
+        # Проверяем, что вернулась 404 ошибка.
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        # Убедимся, что комментарий по-прежнему на месте.
+        comments_count = Comment.objects.count()
+        self.assertEqual(comments_count, 1) 
+
+    def test_author_can_edit_comment(self):
+        # Выполняем запрос на редактирование от имени автора комментария.
+        response = self.author_client.post(self.edit_url, data=self.form_data)
+        # Проверяем, что сработал редирект.
+        self.assertRedirects(response, self.url_to_comments)
+        # Обновляем объект комментария.
+        self.comment.refresh_from_db()
+        # Проверяем, что текст комментария соответствует обновленному.
+        self.assertEqual(self.comment.text, self.NEW_COMMENT_TEXT)
+
+    def test_user_cant_edit_comment_of_another_user(self):
+        # Выполняем запрос на редактирование от имени другого пользователя.
+        response = self.reader_client.post(self.edit_url, data=self.form_data)
+        # Проверяем, что вернулась 404 ошибка.
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        # Обновляем объект комментария.
+        self.comment.refresh_from_db()
+        # Проверяем, что текст остался тем же, что и был.
+        self.assertEqual(self.comment.text, self.COMMENT_TEXT) 
