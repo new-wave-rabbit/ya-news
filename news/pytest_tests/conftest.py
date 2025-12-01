2fb03f3ef8d37534406ca5acd544e174dab6306c
dotsenkois <dotsenkois@yandex.ru>
2025-12-01 09:29:29 +0700

    С горем пополам добавлен тест доступности основных страниц

--- news/pytest_tests/conftest.py
+++ news/pytest_tests/conftest.py
@@ -5,48 +5,80 @@ import pytest
 from django.test.client import Client
 
 # Импортируем модель заметки, чтобы создать экземпляр.
-from notes.models import Note
+from news.models import Comment, News
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 @pytest.fixture
-# Используем встроенную фикстуру для модели пользователей django_user_model.
-def author(django_user_model):  
-    return django_user_model.objects.create(username='Автор')
+def author(db):
+    return User.objects.create(username="Лев Толстой")
 
 
 @pytest.fixture
-def not_author(django_user_model):  
-    return django_user_model.objects.create(username='Не автор')
+def reader(db):
+    return User.objects.create(username="Читатель простой")
 
 
 @pytest.fixture
-def author_client(author):  # Вызываем фикстуру автора.
-    # Создаём новый экземпляр клиента, чтобы не менять глобальный.
-    client = Client()
-    client.force_login(author)  # Логиним автора в клиенте.
-    return client
+def news(db):
+    return News.objects.create(title="Заголовок", text="Текст")
 
 
 @pytest.fixture
-def not_author_client(not_author):
-    client = Client()
-    client.force_login(not_author)  # Логиним обычного пользователя в клиенте.
-    return client
-
-
-@pytest.fixture
-def note(author):
-    note = Note.objects.create(  # Создаём объект заметки.
-        title='Заголовок',
-        text='Текст заметки',
-        slug='note-slug',
+def comment(db, news, author):
+    return Comment.objects.create(
+        news=news,
         author=author,
+        text="Текст комментария"
     )
-    return note
 
-@pytest.fixture
-# Фикстура запрашивает другую фикстуру создания заметки.
-def slug_for_args(note):  
-    # И возвращает кортеж, который содержит slug заметки.
-    # На то, что это кортеж, указывает запятая в конце выражения.
-    return (note.slug,) 
+
+
+# @pytest.fixture
+# # Используем встроенную фикстуру для модели пользователей django_user_model.
+# def author(django_user_model):
+#     return django_user_model.objects.create(username='Автор')
+
+
+# @pytest.fixture
+# def not_author(django_user_model):  
+#     return django_user_model.objects.create(username='Не автор')
+
+
+# @pytest.fixture
+# def author_client(author):  # Вызываем фикстуру автора.
+#     # Создаём новый экземпляр клиента, чтобы не менять глобальный.
+#     client = Client()
+#     client.force_login(author)  # Логиним автора в клиенте.
+#     return client
+
+
+# @pytest.fixture
+# def not_author_client(not_author):
+#     client = Client()
+#     client.force_login(not_author)  # Логиним обычного пользователя в клиенте.
+#     return client
+
+
+# @pytest.fixture
+# def news():
+#     news = News.objects.create(title='Заголовок', text='Текст')
+#     return news
+
+# @pytest.fixture
+# def comment(author):
+#     comment = Comment.objects.create(
+#             news=news(author),
+#             author=author,
+#             text='Текст комментария'
+#         )
+#     return comment
+
+
+# @pytest.fixture
+# # Фикстура запрашивает другую фикстуру создания заметки.
+# def slug_for_args(note):  
+#     # И возвращает кортеж, который содержит slug заметки.
+#     # На то, что это кортеж, указывает запятая в конце выражения.
+#     return (note.slug,) 
--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -7,61 +7,19 @@ from http import HTTPStatus
 
 from django.urls import reverse
 
-
 @pytest.mark.parametrize(
-    'name',  # Имя параметра функции.
-    # Значения, которые будут передаваться в name.
-    ('notes:home', 'users:login', 'users:signup')
-)
-# Указываем имя изменяемого параметра в сигнатуре теста.
-def test_pages_availability_for_anonymous_user(client, name):
-    url = reverse(name)  # Получаем ссылку на нужный адрес.
-    response = client.get(url)  # Выполняем запрос.
-    assert response.status_code == HTTPStatus.OK
-
-@pytest.mark.parametrize(
-    'name',
-    ('notes:list', 'notes:add', 'notes:success')
-)
-def test_pages_availability_for_auth_user(not_author_client, name):
-    url = reverse(name)
-    response = not_author_client.get(url)
-    assert response.status_code == HTTPStatus.OK
-
-@pytest.mark.parametrize(
-    'parametrized_client, expected_status',
+    "name,args",
     [
-        (lf('not_author_client'), HTTPStatus.NOT_FOUND),
-        (lf('author_client'), HTTPStatus.OK)
-    ],
+        ("news:home", None),
+        ("news:detail", pytest.lazy_fixture("news"),),
+        ("users:login", None),
+        ("users:signup", None),
+    ]
 )
-@pytest.mark.parametrize(
-    'name',
-    ('notes:detail', 'notes:edit', 'notes:delete'),
-)
-def test_pages_availability_for_different_users(
-        parametrized_client, name, note, expected_status
-):
-    url = reverse(name, args=(note.slug,))
-    response = parametrized_client.get(url)
-    assert response.status_code == expected_status
-
-@pytest.mark.parametrize(
-    'name, args',
-    (
-        ('notes:detail', pytest.lazy_fixture('slug_for_args')),
-        ('notes:edit', pytest.lazy_fixture('slug_for_args')),
-        ('notes:delete', pytest.lazy_fixture('slug_for_args')),
-        ('notes:add', None),
-        ('notes:success', None),
-        ('notes:list', None),
-    ),
-)
-# Передаём в тест анонимный клиент, name проверяемых страниц и args:
-def test_redirects(client, name, args):
-    login_url = reverse('users:login')
-    # Теперь не надо писать никаких if и можно обойтись одним выражением.
+def test_pages_availability(client, news, name, args):
+    # если не используешь lazy_fixture, можно так:
+    if name == "news:detail":
+        args = (args.id,)
     url = reverse(name, args=args)
-    expected_url = f'{login_url}?next={url}'
     response = client.get(url)
-    assertRedirects(response, expected_url)
+    assert response.status_code == HTTPStatus.OK
